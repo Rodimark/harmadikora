8ad2d8ef969cc274fdb92c9d8cd18921693919ad
Rodimark <rodenbucher.mark@gmail.com>
2025-10-30 11:32:04 +0100

    10.30

--- p08f.py
+++ p08f.py
@@ -3,20 +3,20 @@ from tkinter import messagebox
 import requests
 import p07n
 
-def adatkeres():
+def datakeres():
     try:
-        valasz = requests.get("http://localhost:5000/api/data",timeout=3) #!kerees eseten visszadja a json filet
-        valasz.raise_for_status()
-        adat  = valasz.json()
-        label.config(text=adat["uzenet"])
-        app.throws_num_input.set(adat["uzenet"])
+        response = requests.get("http://localhost:5000/api/data",timeout=3) #!kerees eseten visszadja a json filet
+        response.raise_for_status()
+        data  = response.json()
+        label.config(text=data["uzenet"])
+        app.throws_num_input.set(data["uzenet"])
     except:
         messagebox.showerror("Error", "Nem sikerult a kapcsolat")
 
 root = tk.Tk()
 app= p07n.SaveDiceThrow(root)
 
-api_button = tk.Button(root, text="Lekeres", command = adatkeres) #gomb rootba helyezese
+api_button = tk.Button(root, text="Lekeres", command = datakeres) #gomb rootba helyezese
 api_button.grid(row = 4, column = 0, pady = 10)
 
 label = tk.Label(root, text="...")
